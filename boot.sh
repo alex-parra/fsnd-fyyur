cb31595e7d3c96b7124648b2a69f995d2d18cb62
Alex Parra <alexpds@gmail.com>
2020-05-02 09:54:29 +0100

    Add db-drop to boot.sh

--- boot.sh
+++ boot.sh
@@ -1,21 +1,21 @@
 #! bin/bash
 
+PG_PASS="PGPASSWORD=123456"
+PG_CONN="$PG_PASS psql -h pg -U postgres"
+DB_NAME="fyyur"
+
 initDb() {
-  PG_PASS="PGPASSWORD=123456"
-  PG_CONN="$PG_PASS psql -h pg -U postgres"
-  DB_NAME="fyyur"
   docker-compose run pg bash -c "$PG_CONN -tc \"SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'\" | grep -q 1 || $PG_CONN -c \"CREATE DATABASE $DB_NAME\""
   flask db upgrade
 }
 
+dropDb() {
+  docker-compose run pg bash -c "$PG_PASS dropdb -h pg -U postgres $DB_NAME"
+}
+
 case $1 in
-  db-init)
-    initDb
-    ;;
-  dev)
-    FLASK_APP=app FLASK_ENV=development flask run
-    ;;
-  *)
-    echo 'Unknown mode. Try sh boot.sh dev'
-    ;;
+  db-init) initDb ;;
+  db-drop) dropDb ;;
+  dev) FLASK_APP=app FLASK_ENV=development flask run ;;
+  *) echo 'Unknown mode. Try sh boot.sh dev' ;;
 esac
